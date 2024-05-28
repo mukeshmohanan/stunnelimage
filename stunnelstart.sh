7e2eebd285b0881f8ccd425bff26bf0f41ccb4b2
mukeshmohanan <32169747+mukeshmohanan@users.noreply.github.com>
2024-05-28 07:03:36 +0200

    Update stunnelstart.sh

--- stunnelstart.sh
+++ stunnelstart.sh
@@ -1,6 +1,10 @@
 #set LD directory path
  
 LD_LIBRARY_PATH = /opt/stunnel/bin
+
+# set openssl_conf env variable 
+
+OPENSSL_CNF = /opt/stunnel/conf/openssl.cnf
  
-/opt/stunnel/bin/stunnel /tmp/stunnel.conf
+/opt/stunnel/bin/stunnel /opt/stunnel/conf/stunnel.conf
 
