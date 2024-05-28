2cae7e63cb0b87c0bec5e2cb347ef14425fe423b
root <root@ip-172-31-10-37.eu-north-1.compute.internal>
2024-05-28 10:07:33 +0000

    updated the openssl config variable and the module entry in openssl.cnf

--- stunnel_start.sh
+++ stunnel_start.sh
@@ -2,6 +2,6 @@ set LD directory path
 CONFIG_FILE="/opt/stunnel/config/stunnel.conf"
 export LD_LIBRARY_PATH=/opt/stunnel/lib
 # set openssl_conf env variable
-export OPENSSL_CNF=/opt/stunnel/config/openssl.cnf
+export OPENSSL_CONF=/opt/stunnel/config/openssl.cnf
 # start stunnel process
 /opt/stunnel/bin/stunnel $CONFIG_FILE
