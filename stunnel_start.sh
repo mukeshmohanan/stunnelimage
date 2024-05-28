480d168c7887764b55d9b2633e7591569333dcc9
root <root@ip-172-31-10-37.eu-north-1.compute.internal>
2024-05-28 10:29:48 +0000

    adjusted script file

--- stunnel_start.sh
+++ stunnel_start.sh
@@ -1,5 +1,5 @@
-set LD directory path
-CONFIG_FILE="/opt/stunnel/config/stunnel.conf"
+#set LD directory path
+export CONFIG_FILE="/opt/stunnel/config/stunnel.conf"
 export LD_LIBRARY_PATH=/opt/stunnel/lib
 # set openssl_conf env variable
 export OPENSSL_CONF=/opt/stunnel/config/openssl.cnf
