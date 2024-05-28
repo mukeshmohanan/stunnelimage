c20a7e442e15293f7a5f78270d724c75026f888b
root <root@ip-172-31-10-37.eu-north-1.compute.internal>
2024-05-28 11:23:26 +0000

    sample script

--- script.sh
+++ script.sh
@@ -1,8 +1,2 @@
+#!/bin/sh
 echo "hello"
-#export CONFIG_FILE="/opt/stunnel/config/stunnel.conf"
-#sleep 45
-#export LD_LIBRARY_PATH=/opt/stunnel/lib
-# set openssl_conf env variable
-#export OPENSSL_CONF=/opt/stunnel/config/openssl.cnf
-# start stunnel process
-#/opt/stunnel/bin/stunnel $CONFIG_FILE
